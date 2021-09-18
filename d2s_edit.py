bf81ba998b5421773d430cd6985825948dfe2e8c
pairofdocs <seercthree@gmail.com>
2021-09-18 15:39:10 -0500

    add textbox showing patches to be applied

--- d2s_edit.py
+++ d2s_edit.py
@@ -25,24 +25,34 @@ imglogo = resource_path("d2logo_sm.png")
 
 class Mywin(wx.Frame): 
     def __init__(self, parent, title): 
-        super(Mywin, self).__init__(parent, title = title, size = (380,420))  
+        super(Mywin, self).__init__(parent, title = title, size = (380,460))  
         self.InitUI() 
          
     def InitUI(self):    
         panel = wx.Panel(self) 
         
         # add button to open file
-        btn2 = wx.Button(panel, label = "Open File and Run", pos = (45,30)) 
+        btn2 = wx.Button(panel, label = "Open File and Run", pos = (15,3)) 
         btn2.Bind(wx.EVT_BUTTON, self.OnOpen) 
         self.Centre() 
         self.Show(True)
 
         # add text below button
-        st = wx.StaticText(panel, label="Select a file and apply the edits from patches.txt", pos = (45+3, 30+30))
+        st = wx.StaticText(panel, label="Select a file and apply these edits from patches.txt:", pos=(15+3, 1+30))
+
+        # display uncommented patches
+        with open('patches.txt') as f:
+            lines = f.readlines()
+            uncommented = ""
+            for i in range(1,len(lines)):
+                if lines[i].strip() and lines[i][0] != '#':
+                    uncommented += lines[i-1] + lines[i] + "\n"
+        text = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL, pos=(3, 8+30+20), size=(358,200))
+        text.WriteText(uncommented)
 
         # add image
         png = wx.Image(imglogo, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-        wx.StaticBitmap(self, -1, png, (45+3+16, 30+30+150), (png.GetWidth(), png.GetHeight()))
+        wx.StaticBitmap(self, -1, png, (45+3+16, 30+30+40+185), (png.GetWidth(), png.GetHeight()))
     
     def OnOpen(self, event):
         with wx.FileDialog(self, "Open D2S file", wildcard="D2S files |*.d2s",
